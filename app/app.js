5c4335b6bf1f936b6c2bcc1278f95c34ec9c2b56
Rabiul Islam <rabiul.dev@gmail.com>
2022-08-29 12:49:07 +0600

    Handle error

--- app/app.js
+++ app/app.js
@@ -1,12 +1,31 @@
 require("dotenv").config("../.env");
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
-console.log(process.env.PORT);
+app.use([morgan("dev"), cors(), express.json()]);
 
 app.get("/health", (_req, res) => {
+  throw new Error("Error");
+
   res.status(200).json({ message: "Success" });
+});
+
+app.use((_req, _res, next) => {
+  const error = new Error("Resource Not Found");
+  error.status = 404;
+
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status).json({
+      message: error.message,
+    });
+  }
 });
 
 module.exports = app;
